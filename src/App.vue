9b2f3c267d65e868a438c5c7c15c99bd087a3b63
Regan Ya <hens.virtuella.varld@gmail.com>
2024-05-28 21:31:39 +0200

    feat: do the exercise

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
+import { ref, reactive } from 'vue';
+
+const message = ref('Hello Universe!');
+
+const laptop = reactive({
+  brand: 'Apple',
+  model: 'MacBook Air',
+  chip: 'M3',
+  year: 2024,
+  size: 13,
+  color: 'Space Gray',
+  ram: '8GB',
+  storage: '256GB',
+});
 </script>
 
 <template>
+  <h1>{{ message }}</h1>
+  <p>{{ `${laptop.model} ${laptop.chip} will be an old model in ${laptop.year + 1}`}}</p>
 </template>
-
-<style scoped>
-</style>
+<style scoped></style>
