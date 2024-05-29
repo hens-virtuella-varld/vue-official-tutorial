ebaad65d9837144bfddb349bb9024dc9773a681e
Regan Ya <hens.virtuella.varld@gmail.com>
2024-05-29 22:52:56 +0200

    feat: do the exercise

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+
+const pElementRef = ref(null)
+
+onMounted(()=>pElementRef.value.textContent = 'Mounted and Bye!'
+)
+
 </script>
 
 <template>
+  <p ref="pElementRef">Hello</p>
 </template>
-
-<style scoped>
-</style>
