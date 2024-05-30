a3c1d2e11b41cca81d096045f1084a8e6ce6cbce
Regan Ya <hens.virtuella.varld@gmail.com>
2024-05-30 13:17:55 +0200

    feat: do the exercise

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
+import { ref } from 'vue';
+import ChildComp from './components/ChildComp.vue';
+
+const childMsg = ref('No child msg yet');
 </script>
 
 <template>
+  <ChildComp @response="(msg) => (childMsg = msg)" />
+  <p>{{ childMsg }}</p>
 </template>
-
-<style scoped>
-</style>
